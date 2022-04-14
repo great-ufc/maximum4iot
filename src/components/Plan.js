ebcb10daafde1716f6ac9fd4e28770fb820809a7
Tales Paiva Nogueira <tales.paiva@gmail.com>
2022-04-14 11:09:09 -0300

    plan (list)

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -1,63 +1,30 @@
 import React from 'react';
-import BlockMath from '@matejmazur/react-katex';
 import { useLocation } from 'react-router-dom';
-import Table from './Table';
 
 export default function Plan() {
   let location = useLocation();
-
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Measure',
-        accessor: 'Measure', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Description',
-        accessor: 'Description',
-      },
-      {
-        Header: 'NFR',
-        accessor: 'NFR',
-      },
-      {
-        id: 'Function in Latex',
-        Header: 'Measurement function',
-        //accessor: 'Function in Latex',
-        accessor: (row) => {
-          return (
-            <BlockMath>
-              {String.raw`
-                ${row['Function in Latex']}
-                `}
-            </BlockMath>
-          );
-        },
-      },
-      {
-        Header: 'Interpretation',
-        accessor: 'Interpretation',
-      },
-      {
-        Header: 'Collect method',
-        accessor: 'Collect method',
-      },
-      {
-        Header: 'Reference',
-        accessor: 'Reference',
-      },
-    ],
-    []
-  );
+  const rows = location.state.data;
 
   return (
     <div>
       <h1>Plan...</h1>
-      <Table
-        columns={columns}
-        data={location.state.data}
-      />
-      <pre>{JSON.stringify(location.state, null, 2)}</pre>
+      <table className='table table-hover table-bordered border-secondary align-middle'>
+        {rows.map((row) => {
+          return (
+            <tr>
+              <p>Measure: {row['Measure']}</p>
+              <p>Description: {row['Description']}</p>
+              <p>NFR: {row['NFR']}</p>
+              <p>Measurement function: {row['Measurement function']}</p>
+              <p>Function in Latex: {row['Function in Latex']}</p>
+              <p>Interpretation: {row['Interpretation']}</p>
+              <p>Collect method: {row['Collect method']}</p>
+              <p>Reference: {row['Reference']}</p>
+            </tr>
+          );
+        })}
+      </table>
+      {/* <pre>{JSON.stringify(location.state, null, 2)}</pre> */}
     </div>
   );
 }
